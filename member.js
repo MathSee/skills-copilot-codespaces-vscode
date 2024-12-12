9e1065cd1cb2bce3fd12c40f325dab4ef705bb61
Matheus <matheus_seelend@hotmail.com>
2024-12-12 20:49:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,34 @@
+function skillsMember() {
+  return [
+    {
+      id: 1,
+      name: "HTML",
+      description: "HyperText Markup Language",
+      level: "Intermediate",
+    },
+    {
+      id: 2,
+      name: "CSS",
+      description: "Cascading Style Sheets",
+      level: "Intermediate",
+    },
+    {
+      id: 3,
+      name: "JavaScript",
+      description: "JavaScript",
+      level: "Intermediate",
+    },
+    {
+      id: 4,
+      name: "React",
+      description: "React",
+      level: "Intermediate",
+    },
+    {
+      id: 5,
+      name: "Vue",
+      description: "Vue",
+      level: "Intermediate",
+    },
+  ];
+}
